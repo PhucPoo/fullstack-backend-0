908607909e561ff00d5a6d451169dd52c4985fde
PhucPoo <160504202+PhucPoo@users.noreply.github.com>
2025-07-18 15:04:43 +0700

    video 21

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const path = require('path')
+// console.log('>>>check env', process.env)
+
+require('dotenv').config()
+
 const app = express()
-const port = 8081
+const port = process.env.PORT || 8081
+
+const hostname = process.env.HOST_NAME
+
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
-  res.send('Hello HP vua!')
+  res.send('Hello HP vua! and nodemon')
 })
 app.get('/abc', (req, res) => {
   res.send('Hello !')
@@ -20,6 +29,6 @@ app.get('/sample', (req, res) => {
   res.render('sample.ejs')
 })
 
-app.listen(port, () => {
+app.listen(port,hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
