c57ed405bf77a0c6bd3fe7926e69137d65785815
PhucPoo <160504202+PhucPoo@users.noreply.github.com>
2025-07-21 15:11:11 +0700

    vd 33

--- src/controllers/homeCotroller.js
+++ src/controllers/homeCotroller.js
@@ -1,6 +1,22 @@
-const getHome = (req, res) => {
-     res.send('Hello HP vua! and nodemon')
-}
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const pool = require('../config/database')
+
+
+
+const getHome = ('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    console.table(result.rows);
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Lỗi truy vấn:', err.stack);
+    res.status(500).send('Lỗi server');
+  }
+});
+
+
 
 const getAbc = (req, res) => {
     res.send('Hello !')
--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
+
 require('dotenv').config()
 const express = require('express')
 // console.log('>>>check env', process.env)
 const configViewEngine = require('./config/viewEngine')
 
 const webRoutes = require('./routes/web')
+
+const pool = require('./config/database')
 
 const app = express()
 const port = process.env.PORT || 8081
@@ -13,9 +16,22 @@ const hostname = process.env.HOST_NAME
 configViewEngine(app)
 
 
-app.use('/',webRoutes)
+app.use('/', webRoutes)
 
 
-app.listen(port,hostname, () => {
+pool.connect()
+  .then(() => {
+    console.log('Connected to PostgreSQL');
+    return pool.query('SELECT * FROM users');
+  })
+  .then(res => {
+    console.log('Dữ liệu trong bảng users:');
+    console.table(res.rows);
+  })
+  .catch(err => {
+    console.error('Lỗi kết nối hoặc truy vấn:', err.stack);
+  })
+  
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
